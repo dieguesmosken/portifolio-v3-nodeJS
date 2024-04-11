4628e4360083ec2ace09b9e554f9c555b90191cc
Matheus Mösken Diegues <matheus.diegues@fatec.sp.gov.br>
2024-04-10 21:08:23 -0300

    testando session

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 // Importando o Express (CommonJS Modules)
 const express = require("express")
+// criando uma sessão
+const session = require('express-session')
+
 // Iniciando o express na variável app
 const app = express()
+// Configurar a sessão
+app.use(session
+    ({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: false }
+    }))
+// Rotas de exemplo
+app.get('/session', (req, res) => {
+    // Criar ou recuperar a sessão
+    const sess = req.session;
+  
+    // Definir um valor de sessão
+    sess.views = (sess.views || 0) + 1;
+  
+    // Enviar uma resposta
+    res.send(`Você visitou esta página ${sess.views} vezes.`);
+  });
 
 // Definindo o EJS para renderizar páginas HTML
 app.set('view engine', 'ejs')
@@ -11,8 +33,13 @@ app.use(express.static('public'))
 
 // Criando a primeira rota (ROTA PRINCIPAL)
 app.get("/", (req, res) => {
+    // Criar ou recuperar a sessão
+    const sess = req.session;
+    // Definir um valor de sessão
+    sess.views = (sess.views || 0) + 1;
+    // Enviar uma resposta para a pagina renderizada
+    res.render('index', {views: sess.views})
     // Será renderizada a página 'views\index.ejs'
-    res.render('index')
 })
 // ROTA PERFIL
 app.get("/perfil", (req, res) => { 
